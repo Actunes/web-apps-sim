aacec250d9a6f5ac69c49f1e583f2630a157303c
Igor Forner <igorfornerrodrigues2@gmail.com>
2025-11-10 01:35:41 -0300

    Corrige dependencias cargo

--- webeye-desktop/src-tauri/src/main.rs
+++ webeye-desktop/src-tauri/src/main.rs
@@ -2,16 +2,16 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use tauri::Manager;
-use tauri::api::shell::open;
+use tauri::shell::open;
 use url::Url;
 
 fn main() {
   tauri::Builder::default()
     .setup(|app| {
       
-      let window = app.get_window("main").unwrap();
-      
       let app_handle = app.handle(); 
+      
+      let window = app_handle.get_window("main").unwrap();
 
       window.set_navigation_handler(move |url| {
         let url = Url::parse(&url).unwrap();
@@ -19,7 +19,7 @@ fn main() {
         if url.host_str() == Some("webeye.ivao.aero") {
           return true; 
         } else {
-          open(&app_handle.shell_scope(), url.as_str(), None).unwrap();
+          open(&app_handle, url.as_str(), None).unwrap();
           return false; 
         }
       });
